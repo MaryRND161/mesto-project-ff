7233cf16d4bc68faced555525f885edf79358cef
MaryRND161 <maryrnd161@yandex.ru>
2024-04-19 15:47:08 +0300

    Промежуточный вариант

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,13 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
-const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
-const cardDescription = cardElement.querySelector('.card__description').cloneNode(true);
+const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+const cardDescription = cardItem.querySelector('.card__description').cloneNode(true);
+const cardContent=initialCards.map(function(item) {
+  return{
+    name: item.name,
+    link: item.link
+  };
+});
 
 
 
@@ -9,29 +15,33 @@ const cardDescription = cardElement.querySelector('.card__description').cloneNod
 const content = document.querySelector('.content');
 const placesList = content.querySelector('.places__list');
 const addButton = content.querySelector('.profile__add-button');
-// @todo: Функция создания карточки
-function addCard(linkValue, titleValue) {
 
-  placesItem.querySelector('.card__image').imageContent=linkValue;
-  cardDescription.querySelector('.card__title').textContent=titleValue;
+// @todo: Функция создания карточки
+function createCard({name, link}) {
+  cardItem.querySelector('.card__image').src=link;
+
+  cardDescription.querySelector('.card__title').textContent=name;
 
   cardDescription.querySelector('.card__like-button').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('card__like_active');
-  })
-  placesList.append(placesItem);
+ evt.target.classList.toggle('card__like_active');
+});
+ 
+  //for (let i = 0; i < initialCards.length; i++) {
+  placesList.appendChild(cardItem);
+ // }
 }
 // @todo: Функция удаления карточки
 
+
+
 // @todo: Вывести карточки на страницу
-'const initialCardsCopy=initialCards.map(item=>item);
+function addCard (){
+  cardContent.forEach(createCard);
+}
+
+addCard();
+
+
 addButton.addEventListener('click', function () {
-  const iniLink = document.querySelector('./')
-  initialCardsCopy[0];
 
-  const title = document.querySelector('.input__text_type_title');
-
-  addCard(iniLink.value, title.value);
- 
-  artist.value = '';
-  title.value = '';
 });
