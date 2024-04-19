ad19975eb129e969d6e97837e9abe133b1154f08
MaryRND161 <maryrnd161@yandex.ru>
2024-04-20 00:22:14 +0300

    Проверить по чек-листу

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,5 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
-const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
-const cardDescription = cardItem.querySelector('.card__description').cloneNode(true);
 const cardContent=initialCards.map(function(item) {
   return{
     name: item.name,
@@ -13,35 +11,34 @@ const cardContent=initialCards.map(function(item) {
 
 // @todo: DOM узлы
 const content = document.querySelector('.content');
-const placesList = content.querySelector('.places__list');
 const addButton = content.querySelector('.profile__add-button');
 
 // @todo: Функция создания карточки
-function createCard({name, link}) {
-  cardItem.querySelector('.card__image').src=link;
-
-  cardDescription.querySelector('.card__title').textContent=name;
-
-  cardDescription.querySelector('.card__like-button').addEventListener('click', function (evt) {
+function createCard(cardData,deleteCalback) {
+const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardItem.querySelector('.card__image').src=cardData.link;
+  cardItem.querySelector('.card__title').textContent=cardData.name;
+  cardItem.querySelector('.card__delete-button').addEventListener('click',function(){
+   deleteCalback(cardData);
+  });
+  cardItem.querySelector('.card__like-button').addEventListener('click', function (evt) {
  evt.target.classList.toggle('card__like_active');
 });
- 
-  //for (let i = 0; i < initialCards.length; i++) {
-  placesList.appendChild(cardItem);
- // }
+ return cardItem;
 }
+
 // @todo: Функция удаления карточки
 
 
 
 // @todo: Вывести карточки на страницу
-function addCard (){
-  cardContent.forEach(createCard);
-}
-
-addCard();
-
-
-addButton.addEventListener('click', function () {
-
-});
+function addCard (cardArray){
+  const placesList = content.querySelector('.places__list');
+  cardArray.forEach(function(card){
+    const cardElement=createCard(card,function(cardData){
+      cardElement.remove();
+    });
+  placesList.appendChild(cardElement);
+  });
+};
+addCard(cardContent);
