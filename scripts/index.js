733d3c3205c968d9752459f6fe2727833c7c56e3
MaryRND161 <maryrnd161@yandex.ru>
2024-04-21 00:54:19 +0300

    На проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,33 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
+
+// @todo: DOM узлы
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
+
+// @todo: Функция создания карточки
+function createCard(cardData) {
+const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardItem.querySelector('.card__image').src=cardData.link;
+  cardItem.querySelector('.card__title').textContent=cardData.name;
+  cardItem.querySelector('.card__delete-button').addEventListener('click',deleteCard);
+ return cardItem;
+}
+
+// @todo: Функция удаления карточки
+function deleteCard () {
+  const card = document.querySelector('.card');
+  card.remove();
+  }
+
+// @todo: Вывести карточки на страницу
+function addCard(cardArray) {
+  cardArray.forEach(function(card){
+    const cardElement=createCard(card);
+    placesList.appendChild(cardElement);
+  });
+};
+
 const cardContent=initialCards.map(function(item) {
   return{
     name: item.name,
@@ -7,38 +35,4 @@ const cardContent=initialCards.map(function(item) {
   };
 });
 
-
-
-// @todo: DOM узлы
-const content = document.querySelector('.content');
-const addButton = content.querySelector('.profile__add-button');
-
-// @todo: Функция создания карточки
-function createCard(cardData,deleteCalback) {
-const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardItem.querySelector('.card__image').src=cardData.link;
-  cardItem.querySelector('.card__title').textContent=cardData.name;
-  cardItem.querySelector('.card__delete-button').addEventListener('click',function(){
-   deleteCalback(cardData);
-  });
-  cardItem.querySelector('.card__like-button').addEventListener('click', function (evt) {
- evt.target.classList.toggle('card__like_active');
-});
- return cardItem;
-}
-
-// @todo: Функция удаления карточки
-
-
-
-// @todo: Вывести карточки на страницу
-function addCard (cardArray){
-  const placesList = content.querySelector('.places__list');
-  cardArray.forEach(function(card){
-    const cardElement=createCard(card,function(cardData){
-      cardElement.remove();
-    });
-  placesList.appendChild(cardElement);
-  });
-};
 addCard(cardContent);
